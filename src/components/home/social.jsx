a557bb38adfe962b0adb16ec2c37afc294682977
kuladi03 <kuladi03@gmail.com>
2024-06-28 23:55:35 +0530

    Started Building Personal Portfolio Website

---
+++ src/components/home/social.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://github.com/kuladi03" className="home__social icon" target="_blank">
+                <i class="uil uil-github"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/kuladi/" className="home__social icon" target="_blank">
+                <i class="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://www.instagram.com/adikulkarni03/" className="home__social icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>    
+
+            <a href="https://x.com/Adikulkarni03" className="home__social icon" target="_blank">
+                <i class="uil uil-twitter"></i>
+            </a>    
+        </div>
+    )
+}
+
+export default Social
